eea5054539ae5ee909cddf2fadced1f6a0b8fd6c
Ashton Wiersdorf <ashton.wiersdorf@mailblock.net>
2018-11-27 00:38:41 -0700

    flow working: update code pushed into viewControllerDidShow TODO: make this only do a server check if no array of people exist TODO: pull out update into separate methods so I can call it with a "resync" button somewhere

--- Family Map Client/Controllers/MapViewController.swift
+++ Family Map Client/Controllers/MapViewController.swift
@@ -20,37 +20,49 @@ class MapViewController: UIViewController {
         
         self.title = "Family Map"
 
-//        guard let store = store else {
-//            return
-//        }
-//
-//        let sp = ServerProxy(host: store.host, port: store.port)
-//        print("Host: \(store.host), port: \(store.port)")
-//        print("Auth token: \(store.authToken), root person: \(store.rootPersonID)")
-//        let (ok, message) = sp.getPerson(authToken: store.authToken, personID: store.rootPersonID) {
-//            (ok, resp) in
-//            if let rootPerson = resp as? Person {
-//                self.doAlert("Root Person", message: "\(rootPerson.firstName) \(rootPerson.lastName)")
-//            }
-//        }
-//        if !ok {
-//            print("Problem: \(message)")
-//            return
-//        }
-//
-//        let (ok2, message2) = sp.getPeople(authToken: store.authToken) {
-//            (ok, resp) in
-//            if let people = resp as? [Person] {
-//                store.people = Dictionary(uniqueKeysWithValues: people.map { ($0.personID, $0) })
-//            } else {
-//                print("Response that failed to become an array of people: \(resp)")
-//                self.doAlert("Error", message: "Couldn't parse server response!")
-//            }
-//        }
-//        if !ok2 {
-//            print("Problem: \(message2)")
-//            return
-//        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        print("MapViewController coming into focus...")
+        if let store = store {
+            print("store already initilized")
+            // OK, we can do stuff with store now
+            let sp = ServerProxy(host: store.host, port: store.port)
+            print("Host: \(store.host), port: \(store.port)")
+            print("Auth token: \(store.authToken), root person: \(store.rootPersonID)")
+            let (ok, message) = sp.getPerson(authToken: store.authToken, personID: store.rootPersonID) {
+                (ok, resp) in
+                if let rootPerson = resp as? Person {
+                    self.doAlert("Root Person", message: "\(rootPerson.firstName) \(rootPerson.lastName)")
+                }
+            }
+            if !ok {
+                print("Problem: \(message)")
+                return
+            }
+            
+            let (ok2, message2) = sp.getPeople(authToken: store.authToken) {
+                (ok, resp) in
+                if let people = resp as? [Person] {
+                    store.people = Dictionary(uniqueKeysWithValues: people.map { ($0.personID, $0) })
+                } else {
+                    print("Response that failed to become an array of people: \(resp)")
+                    self.doAlert("Error", message: "Couldn't parse server response!")
+                }
+            }
+            if !ok2 {
+                print("Problem: \(message2)")
+                return
+            }
+
+        }
+        else {
+            print("Augh! WE don't have a data store")
+            self.performSegue(withIdentifier: "doAuth", sender: nil)
+        }
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("MapViewController: here I am!")
     }
 
     func doAlert(_ title: String, message: String) {
