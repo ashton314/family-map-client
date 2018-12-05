96657503b69d8516e9fa5b5e312d264fbbb79bcf
Ashton Wiersdorf <ashton.wiersdorf@mailblock.net>
2018-12-04 23:04:56 -0700

    got a table displaying (woot!)

---
+++ Family Map Client/Backend/SettingToggle.swift
@@ -0,0 +1,19 @@
+//
+//  SettingToggle.swift
+//  Family Map Client
+//
+//  Created by Ashton Wiersdorf on 2018-12-04.
+//  Copyright © 2018 Ashton Wiersdorf. All rights reserved.
+//
+
+import Foundation
+
+class SettingToggle {
+    let title: String
+    let state: Bool
+    
+    init(title: String, state: Bool) {
+        self.title = title
+        self.state = state
+    }
+}
---
+++ Family Map Client/Controllers/SettingsController.swift
@@ -0,0 +1,102 @@
+//
+//  SettingsController.swift
+//  Family Map Client
+//
+//  Created by Ashton Wiersdorf on 2018-12-04.
+//  Copyright © 2018 Ashton Wiersdorf. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UITableViewController {
+
+    var staticSettings = [
+        SettingToggle(title: "Show Lines", state: true),
+        SettingToggle(title: "Show father's side", state: true),
+        SettingToggle(title: "Show mother's side", state: true)
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+
+//        self.navigationItem.rightBarButtonItem =
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return staticSettings.count
+        }
+        else {
+            return 0
+        }
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "staticSettingsCell", for: indexPath)
+        
+        let setting = staticSettings[indexPath.row]
+        cell.textLabel?.text = setting.title
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
